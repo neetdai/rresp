e9fec7fced62c42eebd27882cacd66e2d6b34865
neetdai <neetdai>
2025-02-16 15:43:48 +0800

    Add array decoding benchmarks to v2_decode.rs

--- benches/v2_decode.rs
+++ benches/v2_decode.rs
@@ -2,38 +2,72 @@ use criterion::{black_box, criterion_group, criterion_main, Bencher, BenchmarkId
 use lexical::to_string;
 use rresp::{decode, V2};
 
+fn build_blob(len: usize) -> Vec<u8> {
+    let mut buf = Vec::new();
+    let len_str = to_string(len);
+    buf.push(b'$');
+    buf.extend_from_slice(len_str.as_bytes());
+    buf.extend_from_slice(b"\r\n");
+    buf.extend_from_slice(vec![b'1'; len].as_slice());
+    buf.extend_from_slice(b"\r\n");
+    buf
+}
+
+fn build_array<F>(len: usize, func: F) -> Vec<u8> where F: Fn() -> Vec<u8> {
+    let len_str = to_string(len);
+    let mut buf = Vec::with_capacity(len);
+    buf.push(b'*');
+    buf.extend_from_slice(len_str.as_bytes());
+    buf.extend_from_slice(b"\r\n");
+    for _ in 0..len {
+        let content = func();
+        buf.extend_from_slice(content.as_slice());
+    }
+    buf
+}
+
 struct DecodeBlobParams(Vec<(Vec<u8>, usize)>);
 
 impl DecodeBlobParams {
     fn new() -> Self {
         let params = vec![
-            (Self::build_blob(16), 16),
-            (Self::build_blob(1024), 1024),
-            (Self::build_blob(10240), 10240),
-            (Self::build_blob(102400), 102400),
+            (build_blob(16), 16),
+            (build_blob(1024), 1024),
+            (build_blob(10240), 10240),
+            (build_blob(102400), 102400),
         ];
         Self(params)
     }
-
-    fn build_blob(len: usize) -> Vec<u8> {
-        let mut buf = Vec::new();
-        let len_str = to_string(len);
-        buf.push(b'$');
-        buf.extend_from_slice(len_str.as_bytes());
-        buf.extend_from_slice(b"\r\n");
-        buf.extend_from_slice(vec![b'1'; len].as_slice());
-        buf.extend_from_slice(b"\r\n");
-        buf
-    }
 }
 
+struct DecodeArrayParams(Vec<(Vec<u8>, usize)>);
+
+impl DecodeArrayParams {
+    fn new() -> Self {
+        let params = vec![
+            (build_array(10, || build_blob(16)), 10),
+            (build_array(100, || build_blob(16)), 100),
+            (build_array(1000, || build_blob(16)), 1000),
+            (build_array(10000, || build_blob(16)), 10000),
+        ];
+        Self(params)
+    }
+}
 fn v2_decode(c: &mut Criterion) {
     let blob_params = DecodeBlobParams::new();
+    let array_params = DecodeArrayParams::new();
     let mut group = c.benchmark_group("v2_decode");
     
     for (blob, len) in blob_params.0 {
         group.throughput(Throughput::Elements(len as u64));
         group.bench_with_input(BenchmarkId::new("decode_blob", len), &blob, |b, i| {
+            b.iter(|| decode::<V2>(black_box(i)).unwrap().unwrap());
+        });
+    }
+
+    for (array, len) in array_params.0 {
+        group.throughput(Throughput::Elements(len as u64));
+        group.bench_with_input(BenchmarkId::new("decode_array", len), &array, |b, i| {
             b.iter(|| decode::<V2>(black_box(i)).unwrap().unwrap());
         });
     }
