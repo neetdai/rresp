0efec756cfbc54955f8925a34932010843825f95
neetdai <neetdai>
2025-02-16 19:17:58 +0800

    Refactor Frame encoding and reorder common pub uses

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ mod encode;
 mod v2;
 mod v3;
 
-pub use common::{Error, Parser, ParseIter, Remaining};
+pub use common::{Error, ParseIter, Parser, Remaining};
 pub use decode::decode;
 pub use v2::{Frame, V2};
--- src/v2/frame.rs
+++ src/v2/frame.rs
@@ -1,4 +1,6 @@
-use super::Lexer;
+use lexical::{to_string_with_options, write_integer_options::STANDARD, WriteIntegerOptions, WriteOptions};
+
+use super::{utils::CRLF, Lexer};
 
 #[derive(Debug, PartialEq)]
 pub enum Frame<'a> {
@@ -8,4 +10,29 @@ pub enum Frame<'a> {
     Array(Vec<Frame<'a>>),
     SimpleError(&'a [u8]),
     SimpleString(&'a [u8]),
+}
+
+impl <'a> Frame<'a> {
+    pub fn encode(&self) -> Vec<u8> {
+        match self {
+            Self::Null => {
+                let mut buf = Vec::with_capacity(5);
+                buf.push(b'$');
+                buf.extend_from_slice(b"-1");
+                buf.extend_from_slice(&CRLF);
+                buf
+            },
+            Self::Integer(num) => {
+                let options = WriteIntegerOptions::builder().build().unwrap();
+                let num_str = to_string_with_options(num, &options);
+                let num_str_len = num_str.as_bytes().len();
+                let mut buf = Vec::with_capacity(3 + num_str_len + 2);
+                buf.push(b':');
+                buf.extend_from_slice(num_str.as_bytes());
+                buf.extend_from_slice(&CRLF);
+                buf
+            }
+            _=> todo!()
+        }
+    }
 }
