70dc56882b6a7b1b2f8aeb8a07a601c8460466b4
neetdai <>
2025-03-13 22:49:36 +0800

    Add array tree benchmarks to v3_decode

--- benches/v3_decode.rs
+++ benches/v3_decode.rs
@@ -59,9 +59,35 @@ impl DecodeArrayParams {
     }
 }
 
+fn build_array_tree(len: usize) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(len * 4 + 3);
+    for _ in 0..len {
+        buf.extend_from_slice(b"*1\r\n");
+    }
+
+    buf.extend_from_slice(b"_\r\n");
+
+    buf
+}
+
+struct DecodeArrayTreeParams(Vec<(Vec<u8>, usize)>);
+
+impl DecodeArrayTreeParams {
+    fn new() -> Self {
+        let params = vec![
+            (build_array_tree(10), 10),
+            (build_array_tree(100), 100),
+            (build_array_tree(1000), 1000),
+            // (build_array_tree(10000), 10000),
+        ];
+        Self(params)
+    }
+}
+
 fn v3_decode(c: &mut Criterion) {
     let bulk_params = DecodeBulkParams::new();
     let array_params = DecodeArrayParams::new();
+    let array_tree_params = DecodeArrayTreeParams::new();
 
     let mut group = c.benchmark_group("v3_decode");
 
@@ -75,6 +101,13 @@ fn v3_decode(c: &mut Criterion) {
     for (array, len) in array_params.0 {
         group.throughput(Throughput::Elements(len as u64));
         group.bench_with_input(BenchmarkId::new("decode_array", len), &array, |b, i| {
+            b.iter(|| decode::<V3>(black_box(i)).unwrap().unwrap());
+        });
+    }
+
+    for (array_tree, len) in array_tree_params.0 {
+        group.throughput(Throughput::Elements(len as u64));
+        group.bench_with_input(BenchmarkId::new("decode_array_tree", len), &array_tree, |b, i| {
             b.iter(|| decode::<V3>(black_box(i)).unwrap().unwrap());
         });
     }
