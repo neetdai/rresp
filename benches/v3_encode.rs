cf57dfc64dc5581cae9b2bd47d9d31a642b446d1
neetdai <>
2025-03-26 22:44:12 +0800

    Add v3 encode benchmarks and enhance tests

---
+++ benches/v3_encode.rs
@@ -0,0 +1,43 @@
+use criterion::{
+    black_box, criterion_group, criterion_main, Bencher, BenchmarkId, Criterion, Throughput,
+};
+use lexical::to_string;
+use rand::random;
+use rresp::{
+    encode,
+    v3::{Frame, V3},
+    EncodeLen,
+};
+
+fn v3_encode(c: &mut Criterion) {
+    let mut group = c.benchmark_group("v3_encode");
+
+    group.throughput(Throughput::Elements(11));
+    group.bench_function("bulk_string", |b| {
+        b.iter(|| {
+            let frame = Frame::Bulkstring { data: b"hello", attributes: None };
+            encode::<V3>(black_box(frame)).unwrap();
+        });
+    });
+
+    let frame = Frame::Array { data: vec![Frame::Bulkstring { data: b"hello", attributes: None }], attributes: None };
+    group.throughput(Throughput::Elements(frame.encode_len() as u64));
+    group.bench_function("array", |b| {
+        b.iter(|| {
+            let frame = Frame::Array { data: vec![Frame::Bulkstring { data: b"hello", attributes: None }], attributes: None };
+            encode::<V3>(black_box(frame)).unwrap();
+        });
+    });
+
+    let frame = Frame::Array { data: vec![Frame::Array { data: vec![Frame::Bulkstring { data: b"hello", attributes: None }], attributes: None }], attributes: None };
+    group.throughput(Throughput::Elements(frame.encode_len() as u64));
+    group.bench_function("array2", |b| {
+        b.iter(|| {
+            let frame = Frame::Array { data: vec![Frame::Array { data: vec![Frame::Bulkstring { data: b"hello", attributes: None }], attributes: None }], attributes: None };
+            encode::<V3>(black_box(frame)).unwrap();
+        });
+    });
+}
+
+criterion_group!(benches, v3_encode);
+criterion_main!(benches);
--- tests/v3_encode.rs
+++ tests/v3_encode.rs
@@ -19,4 +19,16 @@ fn v3_encode() {
         encodeed,
         b"|1\r\n+key\r\n+value\r\n+Ok\r\n"
     );
+
+    let frame = Frame::SimpleError { data: b"err", attributes: None };
+    assert_eq!(frame.encode_len(), 6);
+    let encodeed = encode::<V3>(frame).unwrap();
+    assert_eq!(encodeed, b"-err\r\n");
+
+    let frame = Frame::Bulkstring { data: b"hello", attributes: None };
+    assert_eq!(frame.encode_len(), 11);
+    let encodeed = encode::<V3>(frame).unwrap();
+    assert_eq!(encodeed, b"$5\r\nhello\r\n");
+
+    
 }
